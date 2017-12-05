da3ca380d55eb2b9b3132565e1a471fe528b0997
Connor Garriss <connorgarriss@gmail.com>
2017-12-05 15:07:20 -0700

    Added the Employees Model, Scaffolded Creation, Form Validition, and Roles Drop down for both create and edit.

---
+++ Intex-2017/Controllers/EmployeesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Intex_2017.DAL;
+using Intex_2017.Models;
+
+namespace Intex_2017.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private IntexContext db = new IntexContext();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            // Passes the list of roles to the Create View for the dropdown list
+            ViewBag.roleOptions = new SelectList(getRoleOptions(), "Key", "Value");
+
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,EmpFirstName,EmpLastName,EmpAddress1,EmpAddress2,EmpCity,EmpState,EmpZip,EmpEmail,EmpPhone,EmpPassword,Role,Wage,EmpUsername")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Passes the list of roles to the Edit View for a dropdown list
+            ViewBag.roleOptions = new SelectList(getRoleOptions(), "Key", "Value");
+
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,EmpFirstName,EmpLastName,EmpAddress1,EmpAddress2,EmpCity,EmpState,EmpZip,EmpEmail,EmpPhone,EmpPassword,Role,Wage,EmpUsername")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // Returns the role ID and values. Edit this to add or remove roles.
+        private Dictionary<String, String> getRoleOptions()
+        {
+            var roleOptions = new Dictionary<String, String>
+            {
+                { "SysAdmin", "System Admin" },
+                { "SalesAgent", "Sales Agent" },
+                { "LabTech", "Lab Technician" },
+                { "Billing", "Billing Agent" },
+                { "Reports", "Reporting Agent" }
+            };
+
+            return roleOptions;
+        }
+    }
+}
---
+++ Intex-2017/Models/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace Intex_2017.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        [RegularExpression("^[a-zA-Z''-'\\s]{1,40}$", ErrorMessage = "First name must be between 1 and 40 characters, contain only A-Z characters or \' or - symbols")]
+        public String EmpFirstName { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        [RegularExpression("^[a-zA-Z''-'\\s]{1,40}$", ErrorMessage = "Last name must be between 1 and 40 characters, contain only A-Z characters or \' or - symbols")]
+        public String EmpLastName { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public String EmpAddress1 { get; set; }
+
+        public String EmpAddress2 { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public String EmpCity { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public String EmpState { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        [RegularExpression("^(\\d{5}-\\d{4}|\\d{5}|\\d{9})$|^([a-zA-Z]\\d[a-zA-Z] \\d[a-zA-Z]\\d)$", ErrorMessage = "Please enter a valid 5 digit zip code")]
+        public String EmpZip { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter a valid email address")]
+        public String EmpEmail { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        [RegularExpression("^[01]?[- .]?(\\([2-9]\\d{2}\\)|[2-9]\\d{2})[- .]?\\d{3}[- .]?\\d{4}$", ErrorMessage = "Please enter a valid 10-digit phone number in the 555-555-5555 format")]
+        public String EmpPhone { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public String EmpPassword { get; set; }
+
+        public String Role { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public decimal Wage { get; set; }
+
+        [Required(ErrorMessage = "This field cannot be blank")]
+        public String EmpUsername { get; set; }
+    }
+}
